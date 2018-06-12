394bbdc6eee81b9a261403315c18a299788c2de7
Dmitrii Vasilev <beautiful7sun@gmail.com>
2018-06-12 00:44:47 -0700

    wrapup

---
+++ ReduxSimple/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        { title: "Harry Potter", pages: 101 },
+        { title: "Thinking in Java", pages: 200 },
+        { title: "Reducers give us state data", pages: 1 },
+        { title: "Containers are components with connection to state", pages: 3 }
+    ]
+}
